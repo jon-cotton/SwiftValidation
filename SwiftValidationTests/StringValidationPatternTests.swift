1a451c203aa9715676559beafe8cf8b9573193e4
Jon Cotton <joncotton123@googlemail.com>
2016-06-02 00:44:05 +0100

    Added tests for built in regex patterns happy paths

--- SwiftValidationTests/StringValidationPatternTests.swift
+++ SwiftValidationTests/StringValidationPatternTests.swift
@@ -11,6 +11,15 @@ import XCTest
 
 class StringValidationPatternTests: XCTestCase {
 
+    func test_string_validation_alpha_only_regex_pattern_returns_true_when_matching_alpha_only_string() {
+        guard let didMatch = try? StringValidationPattern.alphaOnly.match("aBcDEfGHijklM") else {
+            XCTFail("Expected alpha only string to match alphaOnly StringValidationPattern")
+            return
+        }
+        
+        XCTAssertTrue(didMatch)
+    }
+    
     func test_string_validation_alpha_only_regex_pattern_throws_contains_non_alpha_characters_error_on_failure_to_match() {
         XCTAssertThrowsError(try StringValidationPattern.alphaOnly.match("123")) { error in
             guard let stringValidationError = error as? StringValidationError else {
@@ -25,6 +34,15 @@ class StringValidationPatternTests: XCTestCase {
                 XCTFail("Unexpected StringValidationError type thrown by StringValidationPattern match failure")
             }
         }
+    }
+    
+    func test_string_validation_numeric_only_regex_pattern_returns_true_when_matching_numeric_only_string() {
+        guard let didMatch = try? StringValidationPattern.numericOnly.match("89745873953459029478") else {
+            XCTFail("Expected numeric only string to match numericOnly StringValidationPattern")
+            return
+        }
+        
+        XCTAssertTrue(didMatch)
     }
     
     func test_string_validation_numeric_only_regex_pattern_throws_contains_non_numeric_characters_error_on_failure_to_match() {
@@ -43,6 +61,15 @@ class StringValidationPatternTests: XCTestCase {
         }
     }
     
+    func test_string_validation_alpha_numeric_only_regex_pattern_returns_true_when_matching_alpha_numeric_only_string() {
+        guard let didMatch = try? StringValidationPattern.alphaNumericOnly.match("897458aBcDEfGHijklM73953459") else {
+            XCTFail("Expected alpha numeric only string to match alphaNumericOnly StringValidationPattern")
+            return
+        }
+        
+        XCTAssertTrue(didMatch)
+    }
+    
     func test_string_validation_alpha_numeric_only_regex_pattern_throws_contains_non_alpha_numeric_characters_error_on_failure_to_match() {
         XCTAssertThrowsError(try StringValidationPattern.alphaNumericOnly.match("%$£")) { error in
             guard let stringValidationError = error as? StringValidationError else {
@@ -57,6 +84,15 @@ class StringValidationPatternTests: XCTestCase {
                 XCTFail("Unexpected StringValidationError type thrown by StringValidationPattern match failure")
             }
         }
+    }
+    
+    func test_string_validation_email_regex_pattern_returns_true_when_matching_valid_email_address_string() {
+        guard let didMatch = try? StringValidationPattern.email.match("billy@test.co.uk") else {
+            XCTFail("Expected valid email address string to match email StringValidationPattern")
+            return
+        }
+        
+        XCTAssertTrue(didMatch)
     }
     
     func test_string_validation_email_regex_pattern_throws_contains_not_a_valid_email_address_error_on_failure_to_match() {
