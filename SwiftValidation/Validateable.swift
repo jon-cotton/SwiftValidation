af7ef1d7e8ef6c98d9423a9490563f608dcc6a02
Jon Cotton <joncotton123@googlemail.com>
2016-06-01 11:48:10 +0100

    Added default implementation for Validateable with extension for Optional to allow otpional types to be validated directly

--- SwiftValidation/Validateable.swift
+++ SwiftValidation/Validateable.swift
@@ -13,3 +13,43 @@ public protocol Validateable {
     
     func validValue(validators: ValidatorType...) throws -> ValidatorType.T
 }
+
+extension Validateable where ValidatorType.T == Self {
+    func validValue(validators: [ValidatorType]) throws -> Self {
+        var errors = AggregateError()
+        
+        for validator in validators {
+            do {
+                try validator.isValid(self)
+            } catch {
+                errors.addError(error)
+            }
+        }
+        
+        guard errors.isEmpty else {
+            throw errors
+        }
+        
+        return self
+    }
+    
+    func validValue(validators: ValidatorType...) throws -> Self {
+        return try validValue(validators)
+    }
+}
+
+extension Optional where Wrapped: Validateable, Wrapped.ValidatorType.T == Wrapped {
+    func validValue(validators: [Wrapped.ValidatorType]) throws -> Wrapped {
+        switch self {
+        case .None:
+            throw ValidationError.valueIsNil
+            
+        case .Some(let value):
+            return try value.validValue(validators)
+        }
+    }
+    
+    func validValue(validators: Wrapped.ValidatorType...) throws -> Wrapped {
+        return try validValue(validators)
+    }
+}
