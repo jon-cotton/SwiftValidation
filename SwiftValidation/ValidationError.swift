5d2a894bb5fcc13588de3231222b4548ebcca536
Jon Cotton <joncotton123@googlemail.com>
2016-06-01 11:46:16 +0100

    Added basic Validation Error enum

---
+++ SwiftValidation/ValidationError.swift
@@ -0,0 +1,13 @@
+//
+//  ValidationError.swift
+//  SwiftValidation
+//
+//  Created by Jon on 01/06/2016.
+//  Copyright © 2016 joncotton. All rights reserved.
+//
+
+import Foundation
+
+enum ValidationError: ErrorType {
+    case valueIsNil
+}
