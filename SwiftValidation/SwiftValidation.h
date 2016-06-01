54d0e940806db6cef8026e83d4b08780a7c1fef4
Jon Cotton <joncotton123@googlemail.com>
2016-06-01 11:22:04 +0100

    Initial Commit

---
+++ SwiftValidation/SwiftValidation.h
@@ -0,0 +1,19 @@
+//
+//  SwiftValidation.h
+//  SwiftValidation
+//
+//  Created by Jon on 01/06/2016.
+//  Copyright © 2016 joncotton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftValidation.
+FOUNDATION_EXPORT double SwiftValidationVersionNumber;
+
+//! Project version string for SwiftValidation.
+FOUNDATION_EXPORT const unsigned char SwiftValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftValidation/PublicHeader.h>
+
+
