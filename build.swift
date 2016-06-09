5d5e06aaaa8ee39b1e2581deaf28f0747ecc2a9d
Jon Cotton <joncotton123@googlemail.com>
2016-06-09 01:04:57 +0100

    Added open simulator step to build script to prevent timeout issue

--- build.swift
+++ build.swift
@@ -6,4 +6,5 @@ let scheme = "SwiftValidation"
 let project = "SwiftValidation.xcodeproj"
 let simulator = "iPhone 6S,OS=9.3"
 
+system("open -b com.apple.iphonesimulator")
 system("xcodebuild -scheme \(scheme) -project \(project) -sdk iphonesimulator -destination 'platform=iOS Simulator,name=\(simulator)' build test")
